5856258308a0b2e3f7329d60b8917f13f0d693e1
Thomas Fowler <tfowl95@gmail.com>
2026-05-11 00:22:04 -0500

    removed unused import

--- src/main/java/com/tfowl95/weather_api/WeatherController.java
+++ src/main/java/com/tfowl95/weather_api/WeatherController.java
@@ -1,11 +1,11 @@
 package com.tfowl95.weather_api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.time.Instant;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import java.time.Instant;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
