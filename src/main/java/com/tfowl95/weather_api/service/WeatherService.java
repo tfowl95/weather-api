e6ecf75781fef74d47737b40f862f77dc5f33067
Thomas Fowler <tfowl95@gmail.com>
2026-06-29 22:06:11 -0500

    implemented simple caching with redis

--- src/main/java/com/tfowl95/weather_api/service/WeatherService.java
+++ src/main/java/com/tfowl95/weather_api/service/WeatherService.java
@@ -1,5 +1,6 @@
 package com.tfowl95.weather_api.service;
 
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -10,16 +11,21 @@ import com.tfowl95.weather_api.config.WeatherApiProperties;
 public class WeatherService {
     private final RestClient restClient;
     private final WeatherApiProperties properties;
+    private final StringRedisTemplate redis;
 
-    public WeatherService(RestClient restClient, WeatherApiProperties properties) {
+    public WeatherService(RestClient restClient, WeatherApiProperties properties, StringRedisTemplate redis) {
         this.restClient = restClient;
         this.properties = properties;
+        this.redis = redis;
     }
 
     public String fetchWeather(String location) {
         
 
         // check redis and return if it is there
+        String cacheKey = "weather:cache:" + location;
+        String cachedResponse = redis.opsForValue().get(cacheKey);
+        if (cachedResponse != null) return cachedResponse;
 
         // grab third party info and store it in redis
         String response = restClient.get()
@@ -30,6 +36,8 @@ public class WeatherService {
             .accept(MediaType.APPLICATION_JSON)
             .retrieve()
             .body(String.class);
+            
+        redis.opsForValue().set(cacheKey, response);
         
         return response;
     }
